e809628bc3bfbad4e1b92bc82ee30f90d1ca3b41
Peter Mackay <mackay.pete+git@gmail.com>
2017-04-28 07:48:19 +0100

    Self-document the debugger UI interface mismatch.

--- src/libdecaf/src/decaf_metaldebugger.mm
+++ src/libdecaf/src/decaf_metaldebugger.mm
@@ -2,6 +2,8 @@
 
 #include "decaf_metaldebugger.h"
 
+#include <common/decaf_assert.h>
+
 using namespace decaf;
 
 MetalDebugUiRenderer::~MetalDebugUiRenderer()
@@ -10,10 +12,12 @@ MetalDebugUiRenderer::~MetalDebugUiRenderer()
 
 void MetalDebugUiRenderer::initialise()
 {
+    decaf_abort("Do not call.");
 }
 
 void MetalDebugUiRenderer::draw(unsigned int width, unsigned int height)
 {
+    decaf_abort("Do not call.");
 }
 
 #endif // DECAF_METAL
